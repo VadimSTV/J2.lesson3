33074f973e4b5ec71b96e9a25e15d1e0a9f542a2
Vadim <95380405+VadimSTV@users.noreply.github.com>
2022-10-16 22:18:15 +0300

    Update Fruit.java

--- lesson3/Fruit.java
+++ lesson3/Fruit.java
@@ -3,11 +3,11 @@ package J2.lesson3;
 public class Fruit {
     private float weigth;  //Измерение массы
 
-    public float getWeigth() {   //Метод который высчитывает вес
+    public float getWeigth() {   //Метод для получения значения поля
         return weigth;
     }
 
-    public void setWeigth(float weigth) {
+    public void setWeigth(float weigth) {  //Метод множества устанавливающий значения поля число с плавающей точкой 
         this.weigth = weigth;
     }
 }
