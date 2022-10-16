30f7124447c961a89bf367cdbb5dddb1f9852b35
Vadim <95380405+VadimSTV@users.noreply.github.com>
2022-10-16 22:30:04 +0300

    Update Orange.java

--- lesson3/Orange.java
+++ lesson3/Orange.java
@@ -2,6 +2,6 @@ package J2.lesson3;
 
 public class Orange extends Fruit {  //Наследуемый класс Orange
     public Orange(){
-        super.setWeigth(1.5f);
+        super.setWeigth(1.5f); //Метод который устанавливает значение поля Масса в классе Orange
     }
 }
