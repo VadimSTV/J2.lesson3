bc8370ff073e811deaff4d80400b00698aadd2da
Vadim <95380405+VadimSTV@users.noreply.github.com>
2022-10-16 20:53:52 +0300

    Update Orange.java

--- lesson3/Orange.java
+++ lesson3/Orange.java
@@ -1,6 +1,6 @@
 package J2.lesson3;
 
-public class Orange extends Fruit {
+public class Orange extends Fruit {  //Наследуемый класс Orange
     public Orange(){
         super.setWeigth(1.5f);
     }
